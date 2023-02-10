76e200b702edea4247c54aa5ba063753adb3e4c8
MintyMonster <camerongliddell@gmail.com>
2023-02-10 03:30:40 +0000

    Index mobile compatibility

---
+++ js/links.js
@@ -0,0 +1,23 @@
+function goToHeader(){
+    window.location.href="#header-section";
+}
+
+function goToAboutMe() {
+    window.location.href="#about-me-section";
+}
+
+function goToUnity(){
+    window.location.href="#unity-section";
+}
+
+function goToSoftware(){
+    window.location.href="#software-section";
+}
+
+function goToWebsite(){
+    window.location.href="#website-section";
+}
+
+function goToContact(){
+    window.location.href="#contact-section";
+}
